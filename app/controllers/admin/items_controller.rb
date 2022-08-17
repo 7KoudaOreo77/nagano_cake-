435d474ce5bfbd39e3748c8f69e8d4078edd7cff
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-08-17 03:40:15 +0000

    [Add]edit/item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
+
   end
 
   def new
@@ -8,12 +9,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(admin_item_params)
+    @item = Item.new
     @item.save
     redirect_to admin_items_path
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -21,11 +23,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render edit_admin_item_path
+    end
   end
 
   private
 
   def admin_genre_params
-    params.require(:item).permit(:name, :price)
+    params.require(:item).permit(:name, :price, :image, :introduction)
   end
 end
