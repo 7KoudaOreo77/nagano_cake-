c0633d55f359ba7b4a72e6d4a8b03373e944bbf3
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-08-24 05:12:25 +0000

    [Update]order_detail

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,19 @@ class Admin::OrderDetailsController < ApplicationController
     #@order = Order.find(params[:order_id])
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order.id)
+    @order = @order_detail.order
+    @order_details = @order.order_details
+
+     if @order_detail.making_status == "making"
+       @order.update(status: "making")
+     end
+
+
+     if not @order_details.where.not(making_status: "finish").exists?
+       @order.update(status: "ready")
+     end
+
+    redirect_to admin_order_path(@order.id)
   end
 
   private
