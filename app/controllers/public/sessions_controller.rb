bfcdb9a0d7aa38c2f15dc8dcbc820d7170ea1521
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-08-24 09:37:00 +0000

    [finish]提出した

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-   about_path
+   root_path
   end
 
   before_action :customer_state, only: [:create]
